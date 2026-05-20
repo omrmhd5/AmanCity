ee1c3db0755dc081a74d58aa30944b1cf7afb5cf
omrmhd5 <omrmhd54@gmail.com>
2026-05-20 14:27:21 +0300

    Add missing AppDelegate.swift

---
+++ frontend/ios/Runner/AppDelegate.swift
@@ -0,0 +1,20 @@
+import Flutter
+import UIKit
+import GoogleMaps
+
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Google Maps API Key
+    GMSServices.provideAPIKey("API_KEY_PLACEHOLDER")
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+}
